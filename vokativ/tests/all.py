4994f39c01aa9a3a77179999e93b5c06506b8476
Michal Danilak <mimino.coder@gmail.com>
2014-02-17 01:38:12 +0100

    Added unitests. Added few more suffixes.

--- vokativ/tests/all.py
+++ vokativ/tests/all.py
@@ -15,9 +15,21 @@ class VokativTest(unittest.TestCase):
                 tests.append(line.decode('utf-8').split())
         return tests
 
-    def test_test_man_first_name(self):
+    def test_man_first_name(self):
         for name, vok in self._get_tests('man_first_name_tests'):
             self.assertEqual(vokativ.vokativ(name, woman=False, last_name=False), vok)
+
+    def test_man_last_name(self):
+        for name, vok in self._get_tests('man_last_name_tests'):
+            self.assertEqual(vokativ.vokativ(name, woman=False, last_name=True), vok)
+
+    def test_woman_first_name(self):
+        for name, vok in self._get_tests('woman_first_name_tests'):
+            self.assertEqual(vokativ.vokativ(name, woman=True, last_name=False), vok)
+
+    def test_woman_last_name(self):
+        for name, vok in self._get_tests('woman_last_name_tests'):
+            self.assertEqual(vokativ.vokativ(name, woman=True, last_name=True), vok)
 
 
 if __name__ == '__main__':
