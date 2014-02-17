39b87b355c0d96fd9d3a907dde6a0aa2c819855c
Michal Danilak <mimino.coder@gmail.com>
2014-02-17 22:27:41 +0100

    Fixed strange error in tests for python 3.2.

--- vokativ/tests/all.py
+++ vokativ/tests/all.py
@@ -20,8 +20,10 @@ class VokativTest(unittest.TestCase):
         self.assertEqual(vokativ('Tom'), 'tome')
         self.assertEqual(vokativ('TOM'), 'tome')
         self.assertEqual(vokativ('ToM'), 'tome')
-        self.assertTrue(isinstance(vokativ('Tom'), six.text_type))
-        self.assertTrue(isinstance(vokativ(u'Tom'), six.text_type))
+        result = vokativ('Tom')
+        self.assertTrue(isinstance(result, six.text_type))
+        result = vokativ(u'Tom')
+        self.assertTrue(isinstance(result, six.text_type))
 
     def test_man_first_name(self):
         for name, vok in self._get_tests('man_first_name_tests'):
