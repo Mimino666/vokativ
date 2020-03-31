04725ce8f9e6b6bb1983e9fd73a5015a8a6280e9
Michal Danilak <mimino.coder@gmail.com>
2020-03-31 12:11:24 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,23 +9,21 @@ import io
 
 with io.open('README.md', encoding='utf-8') as f:
     readme = f.read()
-with io.open('LICENSE', encoding='utf-8') as f:
-    license = f.read()
-
 
 setup(
     name='vokativ',
     version='1.2.0',
     description='Declension of Czech names into vocative case.',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Michal Mimino Danilak',
     author_email='michal.danilak@gmail.com',
     url='https://github.com/Mimino666/vokativ',
     keywords='czech name vocative vokativ jmena ceska',
     packages=['vokativ', 'vokativ.tests'],
-    include_package_data = True,
+    include_package_data=True,
     install_requires=['six'],
-    license=license,
+    license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
@@ -33,10 +31,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
